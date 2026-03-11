97f995cbfc68ed6acdec92f3e791888cb587e6c0
Min1809 <min@infinityitsuccess.com>
2026-03-11 14:58:09 +0700

    temp cmmt

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from . import auto_close_timesheet
 from . import irconfigparamenter
 from . import saleorder
 from . import res_users
-# from . import auto_close_timesheet
